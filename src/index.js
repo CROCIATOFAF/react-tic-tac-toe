6e5e1297dfe4ec9c5f126e350610648b98f6f397
Your Name <you@example.com>
2022-08-24 18:37:14 +0400

    feat: adding functional component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className='square'
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -25,6 +20,7 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        // console.log(squares);
         squares[i] = 'X';
         this.setState({ squares: squares });
     }
